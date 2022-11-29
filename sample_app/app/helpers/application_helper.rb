fe879561daafb9d5706ac36126a4a18c8abd000a
obuciina <obuciina@gmail.com>
2022-11-29 13:38:02 +0100

    Change app title

--- sample_app/app/helpers/application_helper.rb
+++ sample_app/app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
 
   #returns the full title on a per-page basis.
   def full_title(page_title = '')
-    base_title = "Sample App"
+    base_title = "What's Happening"
     if page_title.empty?
       base_title
     else
